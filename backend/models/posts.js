a07a37b444c7dfaf12e0aa2c981048b149a26a4d
Deathvks <dylanjesussuarez@gmail.com>
2025-04-02 08:33:44 +0000

    add posts model

---
+++ backend/models/posts.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db.js";
+
+const Post = sequelize.define(
+  "Post",
+  {
+    post_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "user_id",
+      },
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    create_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "posts",
+    timestamps: false,
+  }
+);
+
+export default Exercise;
