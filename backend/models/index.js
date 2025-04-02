60323b9a427dea5c7b588258ddced12a6eb61faa
Deathvks <dylanjesussuarez@gmail.com>
2025-04-02 09:28:39 +0000

    create index model

---
+++ backend/models/index.js
@@ -0,0 +1,50 @@
+import sequelize from "../db";
+
+// models/index.js
+
+import sequelize from '../db.js';
+
+// Import all model definition files
+import User from './users.js';
+import Post from './posts.js';
+import Comment from './comments.js';
+import Exercise from './exercises.js';
+import UserExercise from './userExercises.js';
+import Reward from './rewards.js';
+
+
+// User associations
+User.hasMany(Post, { foreignKey: 'user_id' });
+User.hasMany(Comment, { foreignKey: 'user_id' });
+User.hasMany(UserExercise, { foreignKey: 'user_id' });
+
+// Post associations
+Post.belongsTo(User, { foreignKey: 'user_id' });
+Post.hasMany(Comment, { foreignKey: 'post_id' });
+
+// Comment associations
+Comment.belongsTo(User, { foreignKey: 'user_id' });
+Comment.belongsTo(Post, { foreignKey: 'post_id' });
+
+// Exercise associations
+Exercise.hasMany(UserExercise, { foreignKey: 'exercise_id' });
+Exercise.hasMany(Reward, { foreignKey: 'exercise_id' });
+
+// UserExercise associations (Join Table)
+UserExercise.belongsTo(User, { foreignKey: 'user_id' });
+UserExercise.belongsTo(Exercise, { foreignKey: 'exercise_id' });
+
+// Reward associations
+Reward.belongsTo(Exercise, { foreignKey: 'exercise_id' });
+
+const db = {
+  sequelize, // The Sequelize instance
+  User,
+  Post,
+  Comment,
+  Exercise,
+  UserExercise,
+  Reward
+};
+
+export default db;
