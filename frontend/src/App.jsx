dace5702ca31ba8accff0d47eb132de37d005619
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-04-04 08:38:30 +0100

    addedMapStructure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,17 @@
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import navBar from './components/navBar/navBar'
-import { profilePage } from './pages/profilePage'
-import { landingPage } from './pages/homePage'
-import { loginPage } from './pages/loginPage'
-import { forumPage } from './pages/forumPage'
-import { activityPage } from './pages/activityPage'
-import './App.css'
+// import { useState, useEffect } from 'react'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { NavBar } from './components/navBar/navBar.jsx'
+// import { profilePage } from './pages/profilePage'
+// import { landingPage } from './pages/homePage'
+import { LoginPage } from './pages/loginPage.jsx'
+// import { forumPage } from './pages/forumPage'
+// import { activityPage } from './pages/activityPage'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <navBar />
-        <Routes>
-          <Route>
-            <navBar />
-          </Route>
-        </Routes>
-      </BrowserRouter >
+      <LoginPage />
     </>
   )
 }
