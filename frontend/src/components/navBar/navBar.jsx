19c7e2c12dfcba918482266967c63ddf52555bd6
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-04-07 10:03:03 +0100

    just added current-activity

--- frontend/src/components/navBar/navBar.jsx
+++ frontend/src/components/navBar/navBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
             <ul>
                 <li><Link to="/"><img src={homeIcon} alt="Home" /></Link></li>
                 <li><Link to="/activityPage"><img src={activityIcon} alt="Activity" /></Link></li>
-                <li><Link to="/current-activity"><img src={currentActivityIcon} alt="currentActivity" /></Link></li>
+                <li><Link to="/current-activityPage"><img src={currentActivityIcon} alt="currentActivity" /></Link></li>
                 <li><Link to="/forumPage"><img src={forumIcon} alt="forum" /></Link></li>
                 <li><Link to="/profilePage"><img src={profileIcon} alt="profile" /></Link></li>
             </ul>
