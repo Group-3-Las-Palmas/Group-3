6a1028f353a8dcbf975fa843bbb573b51bc91107
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-04-07 17:16:13 +0100

    modified activity

--- frontend/src/components/activity/activity.jsx
+++ frontend/src/components/activity/activity.jsx
@@ -1,15 +1,15 @@
-import { ActivityContainer, MeditationDesc } from "./activityStyled";
+import { MeditationContainer, MeditationDesc } from "./activityStyled";
 import MeditationIcon from "../../assets/meditation.svg";
 
 export const Activity = () => {
     return (
-        <ActivityContainer>
+        <MeditationContainer>
             <img src={MeditationIcon} alt="meditation" />
             <MeditationDesc>
                 <h4>3 minutes meditation</h4>
                 <h6>To get your mind in the here and now.</h6>
             </MeditationDesc>
             <button><img src="#" alt="favourite" /></button>
-        </ActivityContainer>
+        </MeditationContainer>
     )
 }
--- frontend/src/components/activity/activityStyled.js
+++ frontend/src/components/activity/activityStyled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const ActivityContainer = styled.section`
+export const MeditationContainer = styled.section`
   display: flex;
   flex-direction: row;
   align-items: center;
