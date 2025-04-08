5617f2b59abdc3e6469471ae2b7549c992a2d5c8
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2025-04-08 08:24:13 +0100

    added quotes to the homepage

--- frontend/src/components/quotes/quotesStyled.js
+++ frontend/src/components/quotes/quotesStyled.js
@@ -1,13 +1,27 @@
 import styled from 'styled-components';
 
 export const QuoteCard = styled.div`
-  background-color: #FFF9DB;
   padding: 20px;
   margin-top: 20px;
   border-radius: 12px;
-  font-size: 18px;
+  font-size: 24px;
   font-weight: 600;
-  color: #1f1f1f;
-  max-width: 400px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  color: #1E3220;
+  width: 100%;
+  max-width: 1200px;
+  line-height: 1.1;
+
+  @media (max-width: 1024px) {
+    font-size: 24px;
+    max-width: 90%;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 24px;
+    max-width: 95%;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 24px;
+  }
 `;
