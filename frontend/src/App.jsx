dee3af238ed42f2c3957e7ba771c8a5272560ac4
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2025-04-08 10:04:39 +0100

    changes from github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 // import { useState, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { NavBar } from './components/navBar/navBar.jsx'
 // import { profilePage } from './pages/profilePage.jsx'
 import { LandingPage } from './pages/landingPage.jsx'
 // import { LoginPage } from './pages/landingPage.jsx'
 // import { forumPage } from './pages/forumPage'
-import { ActivityPage } from './pages/activityPage'
+import { ActivityPage } from './pages/activityPage.jsx'
 import './App.scss'
 import { Layout } from './layout/Layout.jsx'
 import { MainPage } from './pages/mainPage.jsx'
-
 
 function App() {
 
   return (
     <>
-      <section className='page-content'>
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/main" element={<MainPage />} />
-            <Route path="/ActivityPage" element={<activityPage />} />
-            <Route path="/current-activity" element={<activityPage />} />
-            <Route path="/forumPage" element={<forumPage />} />
-            <Route path="/profilePage" element={<profilePage />} />
-          </Routes>
-        </BrowserRouter>
-      </section>
+
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LandingPage />} />
+          <Route path="/" element={<Layout />}>
+              <Route path="/ActivityPage" element={<ActivityPage />} />
+              <Route path="/current-activityPage" element={<current-activityPage />} />
+              <Route path="/forumPage" element={<forumPage />} />
+              <Route path="/profilePage" element={<profilePage />} />
+              <Route path="/mainPage" element={<MainPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
