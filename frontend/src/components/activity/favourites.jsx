32d6ad9c51feb34c108cb208257a80979902e408
jcbo <jcbodaw2911@gmail.com>
2025-04-08 10:05:26 +0200

    Component made for Favourite Exercises list

---
+++ frontend/src/components/activity/favourites.jsx
@@ -0,0 +1,33 @@
+import { getFavouriteExercisesByUser } from '../services/userExerciseService';
+
+const FavouriteExercises = ({ userId }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    const fetchFavourites = async () => {
+      try {
+        const data = await getFavouriteExercisesByUser(userId);
+        setFavourites(data);
+      } catch (err) {
+        console.error("Error fetching favourites", err);
+      }
+    };
+
+    fetchFavourites();
+  }, [userId]);
+
+  return (
+    <div className="favourites">
+      <h2>EFavourite Exercises</h2>
+      <ul>
+        {favourites.map((item) => (
+          <li key={item.exercise_id}>
+            <strong>{item.Exercise.title}</strong> - {item.Exercise.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FavouriteExercises;
