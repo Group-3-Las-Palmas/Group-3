53126b76d1a05fa0558ebfb6aec2c4f424cac6b8
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-04-08 09:16:17 +0100

    added favorite button and made some few adjustments

--- frontend/src/components/activity/activity.jsx
+++ frontend/src/components/activity/activity.jsx
@@ -1,5 +1,23 @@
 import { MeditationContainer, MeditationDesc } from "./activityStyled";
 import MeditationIcon from "../../assets/meditation.svg";
+import favoriteIcon from "../../assets/favoriteIcon.svg";
+import unfavoriteIcon from "../../assets/unfavoriteIcon.svg";
+import { useState } from "react";
+
+export const FavoriteButton = () => {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const handleClick = () => {
+        setIsFavorite(!isFavorite);
+    }
+    return (
+        <button onClick={handleClick} style={({ background: "none", border: "none", cursor: "pointer", padding: "0" })}>
+            <img style={({ display: "block", width: "15px", height: "15px"})} src={isFavorite ? favoriteIcon : unfavoriteIcon} alt="favourite" />
+        </button>
+    )
+
+}
+
 
 export const Activity = () => {
     return (
@@ -9,7 +27,7 @@ export const Activity = () => {
                 <h4>3 minutes meditation</h4>
                 <h6>To get your mind in the here and now.</h6>
             </MeditationDesc>
-            <button><img src="#" alt="favourite" /></button>
+            <FavoriteButton />
         </MeditationContainer>
     )
 }
