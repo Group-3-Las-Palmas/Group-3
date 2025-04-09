600a4f9ead4eef0d1324c1973a85b1614b813932
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2025-04-09 10:03:17 +0100

    small fixes to activity page and new photos

--- frontend/src/components/activity/activity.jsx
+++ frontend/src/components/activity/activity.jsx
@@ -1,4 +1,12 @@
-import { MeditationContainer, MeditationDesc, BreathingContainer, BreathingDesc, StretchingContainer, StretchingDesc } from "./activityStyled";
+import {
+  MeditationContainer,
+  MeditationDesc,
+  BreathingContainer,
+  BreathingDesc,
+  StretchingContainer,
+  StretchingDesc
+} from "./activityStyled";
+
 import MeditationIcon from "../../assets/Meditation.svg";
 import favoriteIcon from "../../assets/favoriteIcon.svg";
 import unfavoriteIcon from "../../assets/unfavoriteIcon.svg";
@@ -8,47 +16,61 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const FavoriteButton = () => {
-    const [isFavorite, setIsFavorite] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
 
-    const handleClick = () => {
-        setIsFavorite(!isFavorite);
-    }
-    return (
-        <button onClick={handleClick} style={({ background: "none", border: "none", cursor: "pointer", padding: "0" })}>
-            <img style={({ display: "block", width: "15px", height: "15px" })} src={isFavorite ? favoriteIcon : unfavoriteIcon} alt="favourite" />
-        </button>
-    )
+  const handleClick = () => {
+    setIsFavorite(!isFavorite);
+  };
 
-}
-
+  return (
+    <button
+      onClick={handleClick}
+      style={{ background: "none", border: "none", cursor: "pointer", padding: "0" }}
+    >
+      <img
+        style={{ display: "block", width: "15px", height: "15px" }}
+        src={isFavorite ? favoriteIcon : unfavoriteIcon}
+        alt="favourite"
+      />
+    </button>
+  );
+};
 
 export const Activity = () => {
-    return (
-        <>
-            <MeditationContainer>
-                <img src={MeditationIcon} alt="meditation" />
-                <MeditationDesc>
-                    <Link to="/current-activityPage"><h4>3 minutes meditation exercise</h4></Link>
-                    <h6>To get your mind in the here and now.</h6>
-                </MeditationDesc>
-                <FavoriteButton />
-            </MeditationContainer>
-            <BreathingContainer>
-                <img src={BreathingIcon} alt="breathingExercise" />
-                <BreathingDesc>
-                    <Link to="/current-activityPage"><h4>3 minutes Breathing exercise</h4></Link>
-                    <h6>To get your mind in the here and now.</h6>
-                </BreathingDesc>
-                <FavoriteButton />
-            </BreathingContainer>
-            <StretchingContainer>
-                <img src={streachingIcon} alt="stretchingExercise" />
-                <StretchingDesc>
-                    <Link to="/current-activityPage"><h4>3 minutes Stretching exercise</h4></Link>
-                    <h6>To get your mind in the here and now.</h6>
-                </StretchingDesc>
-                <FavoriteButton />
-            </StretchingContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <MeditationContainer>
+        <img src={MeditationIcon} alt="meditation" />
+        <MeditationDesc>
+          <Link to="/current-activityPage">
+            <h4>3 minutes meditation exercise</h4>
+          </Link>
+          <h6>To get your mind in the here and now.</h6>
+        </MeditationDesc>
+        <FavoriteButton />
+      </MeditationContainer>
+
+      <BreathingContainer>
+        <img src={BreathingIcon} alt="breathingExercise" />
+        <BreathingDesc>
+          <Link to="/current-activityPage">
+            <h4>3 minutes Breathing exercise</h4>
+          </Link>
+          <h6>To get your mind in the here and now.</h6>
+        </BreathingDesc>
+        <FavoriteButton />
+      </BreathingContainer>
+
+      <StretchingContainer>
+        <img src={streachingIcon} alt="stretchingExercise" />
+        <StretchingDesc>
+          <Link to="/current-activityPage">
+            <h4>3 minutes Stretching exercise</h4>
+          </Link>
+          <h6>To get your mind in the here and now.</h6>
+        </StretchingDesc>
+        <FavoriteButton />
+      </StretchingContainer>
+    </>
+  );
+};
--- frontend/src/components/activity/activityStyled.js
+++ frontend/src/components/activity/activityStyled.js
@@ -1,97 +1,121 @@
 import styled from "styled-components";
 
-export const MeditationContainer = styled.section`
+const sharedContainerStyles = `
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
-  width: 100vw;
+  justify-content: space-between;
   background-color: #FFF2D6;
+  width: 90%;
+  max-width: 360px;
+  margin: 12px auto;
+  padding: 16px;
+`;
+
+export const MeditationContainer = styled.section`
+  ${sharedContainerStyles}
 
   img {
-    display: block;
-    width: 100px;
+    width: 50px;
+    height: 50px;
+    object-fit: contain;
   }
 
   h6 {
-    color : #1E3220;
+    color: #1E3220;
+    font-size: 12px;
+    margin-top: 4px;
   }
 `;
 
 export const MeditationDesc = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  background-color: #FFF2D6;
-  border-radius: 10px;
-  
+  flex: 1;
+  margin-left: 16px;
+
   a {
     text-decoration: none;
     color: #1E3220;
   }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+  }
 `;
 
 export const BreathingContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  background-color: #FFF2D6;
+  ${sharedContainerStyles}
 
   img {
-    display: block;
-    width: 100px;
+    width: 50px;
+    height: 50px;
+    object-fit: contain;
   }
 
   h6 {
-    color : #1E3220;
+    color: #1E3220;
+    font-size: 12px;
+    margin-top: 4px;
   }
 `;
 
 export const BreathingDesc = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  background-color: #FFF2D6;
-  border-radius: 10px;
-  
+  flex: 1;
+  margin-left: 16px;
+
   a {
     text-decoration: none;
     color: #1E3220;
   }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+  }
 `;
 
 export const StretchingContainer = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  background-color: #FFF2D6;
+  ${sharedContainerStyles}
 
   img {
-    display: block;
-    width: 100px;
+    width: 50px;
+    height: 50px;
+    object-fit: contain;
   }
 
   h6 {
-    color : #1E3220;
+    color: #1E3220;
+    font-size: 12px;
+    margin-top: 4px;
   }
 `;
 
 export const StretchingDesc = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
-  background-color: #FFF2D6;
-  border-radius: 10px;
-  
+  flex: 1;
+  margin-left: 16px;
+
   a {
     text-decoration: none;
     color: #1E3220;
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
   }
 `;
