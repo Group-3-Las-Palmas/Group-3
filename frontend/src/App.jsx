7b926fdc6a1638f683311706abcdbb92bbd81a1b
Deathvks <dylanjesussuarez@gmail.com>
2025-04-09 13:42:37 +0000

    add verification token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UserProfilePage from './pages/profilePage.jsx'
-import { LandingPage } from './pages/landingPage.jsx'
-import { SettingsUserPage } from './pages/settingsUserPage.jsx'
-import { ActivityPage } from './pages/activityPage.jsx'
-import { Layout } from './layout/Layout.jsx'
-import { MainPage } from './pages/mainPage.jsx'
-// import { forumPage } from './pages/forumPage'
+// frontend/src/App.jsx
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Añadir Navigate
+import UserProfilePage from './pages/profilePage.jsx';
+import { LandingPage } from './pages/landingPage.jsx';
+import { SettingsUserPage } from './pages/settingsUserPage.jsx';
+import { ActivityPage } from './pages/activityPage.jsx';
+import { Layout } from './layout/Layout.jsx';
+import { MainPage } from './pages/mainPage.jsx';
+import { CurrentActivityPage } from './pages/currentActivityPage.jsx';
 
-import './App.scss'
-import './background.scss'
-import { CurrentActivityPage } from './pages/currentActivityPage.jsx'
+// Importar el componente de protección
+import ProtectedRoute from './components/auth/ProtectedRoute.jsx'; // Ajusta la ruta si es necesario
+
+import './App.scss';
+import './background.scss';
 
 function App() {
   return (
@@ -22,19 +25,24 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route index element={<LandingPage />} />
-            <Route path="/" element={<Layout />}>
-              <Route path="/ActivityPage" element={<ActivityPage />} />
-              <Route path="/forumPage" element={<div>Forum Page</div>} />
-              <Route path="/profilePage" element={<UserProfilePage />} />
-              <Route path="/settingsUserPage" element={<SettingsUserPage />} />
-              <Route path="/current-activityPage" element={<CurrentActivityPage />} />
-              <Route path="/mainPage" element={<MainPage />} />
+
+            <Route element={<ProtectedRoute />}>
+              <Route element={<Layout />}>
+                <Route path="/mainPage" element={<MainPage />} />
+                <Route path="/activityPage" element={<ActivityPage />} />
+                <Route path="/forumPage" element={<div>Forum Page</div>} />
+                <Route path="/profilePage" element={<UserProfilePage />} />
+                <Route path="/settingsUserPage" element={<SettingsUserPage />} />
+                <Route path="/current-activityPage" element={<CurrentActivityPage />} />
+                <Route path="/" element={<Navigate to="/mainPage" replace />} />
+              </Route>
             </Route>
+
           </Routes>
         </BrowserRouter>
       </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
